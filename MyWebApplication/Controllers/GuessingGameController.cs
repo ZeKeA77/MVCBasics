b03572dac1cb9945511c02d327fad51adc090bee
ZeKeA77 <raul@cobos.se>
2022-08-22 11:31:47 +0200

    Assignment-2

---
+++ MyWebApplication/Controllers/GuessingGameController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApplication.Controllers
+{
+    public class GuessingGameController : Controller
+    {
+        public IActionResult GuessGame()
+        {
+            return View();
+        }
+    }
+}
